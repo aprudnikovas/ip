a1251358032352889734d3adb2c8a3be212e8849
Ivar Prudnikov <aprudnikovas@yahoo.com>
2013-11-01 19:48:29 +0000

    working on pagination of projects

---
+++ client-dev/scripts/factories/projects.js
@@ -0,0 +1,30 @@
+angular.module('tApp')
+	.factory('WorkData', ['$http', function ($http) {
+
+		var cachedData = null;
+
+		var successFn = function(resp){
+
+			// resp has data,status,header,config
+
+			cachedData = resp.data;
+			return cachedData;
+		};
+		var errorFn = function(resp){
+
+			console.error("error code:" + resp.status)
+
+			return cachedData;
+		};
+
+		var json = $http.get('/work_data.json').then(successFn,errorFn);
+
+		return {
+			data: function(){
+				if(cachedData != null)
+					return cachedData;
+				return json;
+			}
+		}
+
+	}]);
