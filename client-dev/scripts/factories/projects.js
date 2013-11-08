1e9834f6f72be936468a35b5cd26a587bf50fff9
Ivar Prudnikov <aprudnikovas@yahoo.com>
2013-11-08 00:26:35 +0000

    in progress. Adding portfolio filtering

--- client-dev/scripts/factories/projects.js
+++ client-dev/scripts/factories/projects.js
@@ -1,23 +1,27 @@
 angular.module('tApp')
-	.factory('Projects', ['$http', function ($http) {
+	.factory('Projects', ['$resource', function ($resource) {
 
-		var cachedData = null;
+		return $resource('/data/projects.json', {}, {
+			getAll: { method:'GET', isArray: true }
+		});
 
-		var errorHandler = function(resp){
-			// data,status,header,config
-			throw new Error("Something bad happened. Data:" + resp.data);
-		};
-
-		var getData = function(callback){
-			return $http.get('/data/projects.json')
-				.then(function(resp){
-					//data,status,header,config
-					return cachedData = cachedData || resp.data;
-				}, errorHandler)
-		}
-
-		return {
-			data: getData
-		}
+//		var cachedData = null;
+//
+//		var errorHandler = function(resp){
+//			// data,status,header,config
+//			throw new Error("Something bad happened. Data:" + resp.data);
+//		};
+//
+//		var getData = function(callback){
+//			return $http.get('/data/projects.json')
+//				.then(function(resp){
+//					//data,status,header,config
+//					return cachedData = cachedData || resp.data;
+//				}, errorHandler)
+//		}
+//
+//		return {
+//			data: getData
+//		}
 
 	}]);
