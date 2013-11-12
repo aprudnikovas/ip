44a2b5e2f62fa93aad4233b517bdce7bad97b449
Ivar Prudnikov <aprudnikovas@yahoo.com>
2013-11-12 23:33:29 +0000

    added skills/languages filters

--- client-dev/scripts/controllers/workGallery.js
+++ client-dev/scripts/controllers/workGallery.js
@@ -1,11 +1,11 @@
 angular.module('tApp')
-	.controller('WorkGalleryController', ['Projects', 'Companies','$scope','$rootScope','$timeout','$stateParams','$location',
-		function (Projects,Companies,$scope,$rootScope,$timeout,$stateParams,$location) {
+	.controller('WorkGalleryController', ['Projects', 'Companies','Skills','Languages','$scope','$rootScope','$timeout','$stateParams','$location',
+		function (Projects,Companies,Skills,Languages,$scope,$rootScope,$timeout,$stateParams,$location) {
 
 			var
 				codeTimer, DEFAULT_MAX, DEFAULT_OFFSET,
 				searchFilter, pagination, params,
-				allProjects, years
+				allProjects, allCompanies, allSkills, allLanguages, years
 				;
 
 			DEFAULT_MAX = 6;
@@ -34,7 +34,8 @@ angular.module('tApp')
 			searchFilter = $scope.searchFilter = {
 				company_ids : [],
 				years : [],
-				tags : [],
+				skill_ids : [],
+				language_ids : [],
 				ttt : null // when it is necessary to trigger change
 			};
 
@@ -49,7 +50,9 @@ angular.module('tApp')
 			// watch filter and update model
 			////////////////////////////////////////////
 
-			$scope.companies = Companies.getAll();
+			allCompanies = $scope.companies = Companies.getAll();
+			allSkills = $scope.skills = Skills.getAll();
+			allLanguages = $scope.languages = Languages.getAll();
 			allProjects = Projects.getAll(filterAndSortPortfolio);
 
 			$scope.$watch( 'searchFilter', filterAndSortPortfolio, true);
@@ -60,12 +63,11 @@ angular.module('tApp')
 
 				// filter
 				if(searchFilter.company_ids.length){
-
 					filteredByProperty = _.filter(filteredByProperty, function(proj){
-						return _.contains(searchFilter.company_ids, parseInt(proj.company_id) )
+						return _.contains(searchFilter.company_ids, proj.company_id )
 					});
-
 				}
+
 				if(searchFilter.years.length){
 
 					filteredByProperty = _.filter(filteredByProperty, function(proj){
@@ -103,6 +105,22 @@ angular.module('tApp')
 
 				}
 
+				if(searchFilter.skill_ids.length){
+					filteredByProperty = _.filter(filteredByProperty, function(proj){
+						return _.some(searchFilter.skill_ids, function(filterSkillId){
+							return _.contains(proj.skills, filterSkillId )
+						})
+					});
+				}
+
+				if(searchFilter.language_ids.length){
+					filteredByProperty = _.filter(filteredByProperty, function(proj){
+						return _.some(searchFilter.language_ids, function(filterLanguageId){
+							return _.contains(proj.languages, filterLanguageId )
+						})
+					});
+				}
+
 				// sort
 
 				// save pagination details
@@ -122,12 +140,35 @@ angular.module('tApp')
 
 			$scope.toggleCompany = function(companyId){
 
-				// TODO add toggleCompany filter function
-				////////////////////////////////////////////
-
 				// RESET MAX & OFFSET
 				pagination.offset = DEFAULT_OFFSET
 				pagination.max = DEFAULT_MAX
+
+				if(companyId != null){
+					_.each(allCompanies, function(obj, index, list){
+						if(obj.id === companyId){
+
+							// change state
+							obj.selected = !(!!obj.selected)
+
+							// update search filter
+							if( _.indexOf(searchFilter.company_ids, obj.id) > -1 ){
+								searchFilter.company_ids = _.difference(searchFilter.company_ids, [obj.id])
+							} else {
+								searchFilter.company_ids = _.union(searchFilter.company_ids, [obj.id])
+							}
+
+						}
+					})
+				} else {
+					_.each(allCompanies, function(obj, index, list){
+						obj.selected = false;
+					})
+					searchFilter.company_ids = [];
+				}
+
+				// TRIGGER CHANGE
+				searchFilter.ttt = (new Date).getTime()
 
 			}
 
@@ -163,7 +204,73 @@ angular.module('tApp')
 				searchFilter.ttt = (new Date).getTime()
 			}
 
+			$scope.toggleSkill = function(id){
 
+				// RESET MAX & OFFSET
+				pagination.offset = DEFAULT_OFFSET
+				pagination.max = DEFAULT_MAX
+
+				if(id != null){
+					_.each(allSkills, function(obj, index, list){
+						if(obj.id === id){
+
+							// change state
+							obj.selected = !(!!obj.selected)
+
+							// update search filter
+							if( _.indexOf(searchFilter.skill_ids, obj.id) > -1 ){
+								searchFilter.skill_ids = _.difference(searchFilter.skill_ids, [obj.id])
+							} else {
+								searchFilter.skill_ids = _.union(searchFilter.skill_ids, [obj.id])
+							}
+
+						}
+					})
+				} else {
+					_.each(allSkills, function(obj, index, list){
+						obj.selected = false;
+					})
+					searchFilter.skill_ids = [];
+				}
+
+				// TRIGGER CHANGE
+				searchFilter.ttt = (new Date).getTime()
+
+			}
+
+			$scope.toggleLanguage = function(id){
+
+				// RESET MAX & OFFSET
+				pagination.offset = DEFAULT_OFFSET
+				pagination.max = DEFAULT_MAX
+
+				if(id != null){
+					_.each(allLanguages, function(obj, index, list){
+						if(obj.id === id){
+
+							// change state
+							obj.selected = !(!!obj.selected)
+
+							// update search filter
+							if( _.indexOf(searchFilter.language_ids, obj.id) > -1 ){
+								searchFilter.language_ids = _.difference(searchFilter.language_ids, [obj.id])
+							} else {
+								searchFilter.language_ids = _.union(searchFilter.language_ids, [obj.id])
+							}
+
+						}
+					})
+				} else {
+					_.each(allLanguages, function(obj, index, list){
+						obj.selected = false;
+					})
+					searchFilter.language_ids = [];
+				}
+
+				// TRIGGER CHANGE
+				searchFilter.ttt = (new Date).getTime()
+
+			}
 
 
 			// public methods allowing pagination
