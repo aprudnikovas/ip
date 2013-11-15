1831573105036c1bef40bfb1d7f817ce77b82fc1
Ivar Prudnikov <aprudnikovas@yahoo.com>
2013-11-15 20:18:26 +0000

    added search filter wrapper directive

--- client-dev/scripts/directives/directives.js
+++ client-dev/scripts/directives/directives.js
@@ -1,6 +1,26 @@
 angular.module('tApp')
-	.directive('FilterDropdown', ['$scope','$rootScope','$timeout', function ($scope,$rootScope,$timeout) {
 
+/**
+ * Dropdown directive that gets opened and closed only through
+ * clicking an opener
+ * Returns <li></li> element
+ */
+	.directive('filterdropdown', function () {
+		return {
+			restrict: 'EA',
+			template: '<li class="filterdropdown_root" ng-class="{filterdropdown_open:active}">'+
+				'<a class="filterdropdown_link" ng-click="toggle()">{{title}} <span class="caret"></span></a>'+
+				'<div class="filterdropdown_dropdown" ng-transclude></div>'+
+				'</li>',
+			scope: { title: '@', active:'&' },
+			controller: ['$scope', '$element', '$transclude',function($scope,$element,$transclude){
+				$scope.active = false;
+				$scope.toggle = function(){
+					$scope.active = !$scope.active;
+				}
+			}],
+			replace: true,
+			transclude: true
+		};
 
-
-	}]);
+	});
