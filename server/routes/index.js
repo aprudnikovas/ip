59b0808e8eb0851e9c95cd43916eee6bda81d658
Ivar Prudnikov <aprudnikovas@yahoo.com>
2014-01-16 23:17:34 +0000

    hire form additional validation and posting to server

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,6 +19,27 @@
 
 		});
 
+		app.post('/sendemail', function (req, res) {
+
+			var data = {
+				sent:false
+			};
+
+			console.log("****************")
+			console.log(req.body)
+			console.log("****************")
+
+			return res.format({
+				html: function(){
+					res.render('index.html');
+				},
+				json: function(){
+					res.send(data);
+				}
+			});
+
+		});
+
 	};
 
 }(exports));
