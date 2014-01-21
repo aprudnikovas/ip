6a55c13aa7efccd2b9f4b237cafb56165eccb18e
Ivar Prudnikov <aprudnikovas@yahoo.com>
2014-01-21 21:51:48 +0000

    updated postemail for testing

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,13 @@
+var AWS = require('aws-sdk');
+AWS.config.region = 'eu-west-1';
+
 (function (exports) {
 
 	"use strict";
 
 	exports.init = function (app) {
+
+		var ses = new AWS.SES();
 
 		app.get('/', function (req, res) {
 
@@ -21,18 +26,54 @@
 
 		app.post('/sendemail', function (req, res) {
 
-			var data = {
+			var response = {
 				sent:false
 			};
 
-			return res.format({
-				html: function(){
-					res.render('index.html');
-				},
-				json: function(){
-					res.send(data);
-				}
-			});
+			ses.listIdentities({}, function(err,data){
+
+				response.idtt = data;
+				response.idttErr = err;
+
+				ses.sendEmail({
+					Source:'info@ivarprudnikov.com',
+					Destination:{
+						ToAddresses:["ivar.prudnikov@gmail.com"]
+					},
+					Message: {
+						Subject: {
+							Data: req.body.subject
+						},
+						Body: {
+							Html: {
+								Data: req.body.message
+							}
+						}
+					},
+					ReplyToAddresses: [req.body.email]
+				}, function(error,responseData){
+					if(error == null){
+						response.sent = true;
+						response.successData = responseData;
+					} else {
+						response.error = error;
+					}
+
+					return res.format({
+						html: function(){
+							res.render('index.html');
+						},
+						json: function(){
+							res.send(response);
+						}
+					});
+				}); // end ses send
+
+
+			}); // end ses list
+
+
+
 
 		});
 
